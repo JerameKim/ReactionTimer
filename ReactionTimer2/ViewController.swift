0733be4784a1b0215131f98069f778bd06f585e6
JerameKim <jkidwin@gmail.com>
2020-06-03 16:27:03 -0700

    First commit, reaction timer works

--- ReactionTimer2/ViewController.swift
+++ ReactionTimer2/ViewController.swift
@@ -9,10 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var startDate = Date()
+    var endDate = Date()
+    
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var endButton: UIButton!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.setTitle("", for: .normal)
+        endButton.setTitle("", for: .normal)
+        
+        // on button press set button color to blue
+        startButton.backgroundColor = UIColor.blue
+        startButton.setTitleColor(.black, for: .normal)
+        endButton.isHidden = true
+    }
+    
+    @IBAction func startPressed(_ sender: Any) {
+        // on button press set screen/button color to red
+        startButton.backgroundColor = UIColor.red
+        
+        // hide the start message
+        messageLabel.isHidden = true
+        // create a random countdown for the trigger to be started
+        let triggerTime = Double.random(in: 3...6)
+        print(triggerTime)
+        
+        // do gameStart() 4seconds after the butotn has been pressed
+        perform(#selector(gameStart), with: nil, afterDelay: triggerTime)
+        
+    }
+    
+    @IBAction func endPressed(_ sender: Any) {
+        endButton.backgroundColor = UIColor.white
+        endDate = Date()
+        
+        let elapsedTime = endDate.timeIntervalSince(startDate)
+        messageLabel.isHidden = false
+        messageLabel.text = "Your reaction time was \(elapsedTime)s"
+    }
+    
+    // gets called after delay
+    @objc func gameStart() {
+        startButton.isHidden = true
+        endButton.isHidden = false
+        endButton.backgroundColor = UIColor.green
+        
+        startDate = Date()
+        
+        
     }
 
 
